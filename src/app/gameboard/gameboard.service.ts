ddc914e5ef185a9eeee290fcc74e57ac866268e4
unknown <nate.lowe.2013@owu.edu>
2021-01-04 01:07:32 -0500

    GB component to service link works

--- src/app/gameboard/gameboard.service.ts
+++ src/app/gameboard/gameboard.service.ts
@@ -1,16 +1,23 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Input } from '@angular/core';
+import { Buffer } from '../models/buffer';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GameboardService {
-
+  public buffer = Buffer;
   public boardConstants: String[] = ["BD","55","E9","FF","7A","1C","D5"];
 
   constructor() { }
 
   initializeBoard(){
 
+  }
+
+  @Input()
+  initializeBuffer(buffer: Buffer){
+    buffer.score = 0;
+    return buffer;
   }
 
   produceBuffer(){
